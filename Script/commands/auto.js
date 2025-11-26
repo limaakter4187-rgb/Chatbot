cbc95bd29c4318ae870d8e3625da1911b4a571c8
limaakter4187-rgb <limaakter4187@gmail.com>
2025-11-26 11:13:08 +0600

    Update auto.js

--- Script/commands/auto.js
+++ Script/commands/auto.js
@@ -29,8 +29,8 @@ const data = await alldown(content);
  fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
 
  return api.sendMessage({
- body: `🔥🚀 𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁 | ᵁᴸᴸ⁴ˢᴴ 🔥💻 
-📥⚡𝗔𝘂𝘁𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿⚡📂
+ body: `🔥🚀 𝗢𝗛𝗜𝗗 𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 | 𝗢𝗛𝗜𝗗 🔥💻 
+📥⚡𝗔𝘂𝘁𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿
 🎬 𝐄𝐧𝐣𝐨𝐲 𝐭𝐡𝐞 𝐕𝐢𝐝𝐞𝐨 🎀`,
  attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
 
